3a36a62ffd50bb48c99b11af7d337787744ddf36
ozz1e <806199629@qq.com>
2021-08-12 23:40:06 +0800

    新增以及修改模板文件

--- app/Admin/Controllers/TemplateController.php
+++ app/Admin/Controllers/TemplateController.php
@@ -12,6 +12,7 @@ use Dcat\Admin\Grid;
 use Dcat\Admin\Layout\Content;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
 
 class TemplateController extends  AdminController
 {
@@ -56,7 +57,6 @@ class TemplateController extends  AdminController
             $form->select('type')->required()->options([1=>'文章模板',2=>'AMP模板']);
             $form->file('file_path')->required()->accept('html')->help('只能上传.html格式的文件，如果是上传AMP模板请以.amp.html结尾');
 
-
             //去掉底部查看按钮
             $form->disableViewCheck();
             //去掉继续编辑
@@ -85,6 +85,17 @@ class TemplateController extends  AdminController
         $form->disableCreatingCheck();
         //去掉重置创建
         $form->disableResetButton();
+        $form->tools(function (Form\Tools $tools) {
+            // 去掉跳转列表按钮
+            $tools->disableList();
+            // 去掉跳转详情页按钮
+            $tools->disableView();
+            // 去掉删除按钮
+            $tools->disableDelete();
+
+            // 添加一个按钮, 参数可以是字符串, 匿名函数, 或者实现了Renderable或Htmlable接口的对象实例
+            $tools->append('<a class="btn btn-sm btn-primary" style="color: #fff;" href="'.url('admin/template').'"><i class="feather icon-list"></i>&nbsp;&nbsp;列表</a>');
+        });
         $templateId = $request->route()->id;
         $form->action('template/'.$templateId.'/saveTemplate');
         $form->confirm('您确定要保存编辑内容吗？', '保存后会直接覆盖原文件，请谨慎操作！');
@@ -102,6 +113,7 @@ class TemplateController extends  AdminController
             }
         }
 
+
         $form->php('code',$title)->help('若未找到文件则显示空白,点击保存仍会生成相应内容的html文件')->value($fileContent);
         return $content
             ->header('在线修改模板文件')
@@ -110,16 +122,30 @@ class TemplateController extends  AdminController
 
     public function saveTemplate(Request $request)
     {
-//        dd($request->all());
+        $data = $request->post();
+        $form = new Form();
         $id = $request->route('id');
         if( is_numeric($id) ){
             $template = \App\Models\Template::select(['file_path'])->find($id);
             if( !is_null($template) ){
                 $templateArr = $template->toArray();
-                $title = $templateArr['temp_name'];
+                //文件本地上传的路径
+                $fileUploadPath = config('filesystems.disks.admin.root').DIRECTORY_SEPARATOR.config('admin.upload.directory.file');
+                if( !is_dir($fileUploadPath) ){
+                    mkdir($fileUploadPath,0777,true);
+                }
                 $filePath = public_path('uploads'.DIRECTORY_SEPARATOR).$templateArr['file_path'];
-
+                $result = file_put_contents($filePath,$data['code']?:'',LOCK_EX);
+                if( $result >= 0 ){
+                    return $form->response()->success('保存成功');
+                }else{
+                    return $form->response()->error('保存失败')->refresh();
+                }
+            }else{
+                return $form->response()->error('数据有误，请稍后再试')->refresh();
             }
+        }else{
+            return $form->response()->redirect('template');
         }
     }
 
